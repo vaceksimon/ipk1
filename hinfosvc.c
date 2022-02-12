e1341161d2d98e181fa67316e840ca8a4e821779
Šimon Vacek <simonvacky@email.cz>
2022-02-12 11:06:09 +0100

    fixed reading cpu name

--- hinfosvc.c
+++ hinfosvc.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -17,26 +18,39 @@
 
 #define STRLEN 100
 
+/**
+ * @brief Ze souboru /proc/cpuinfo precte jmeno CPU.
+ *
+ * @param name String, do ktereho ulozi jmeno CPU.
+*/
 int getcpuname(char *name) {
   FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+  if(cpuinfo == NULL)
+    return 1;
   char *TARGET = "model name";
 
   while(1) {
     int i = 0;
     char k;
+
+    // v textu hledam TARGET
     while(fgetc(cpuinfo) == TARGET[i])
       i++;
-    if(i == sizeof(TARGET) + 2) {
+
+    // TARGET se v textu nachazi
+    if((size_t) i == strlen(TARGET)) {
       do{k = fgetc(cpuinfo);}while(k != ':'); 
       i = 0;
-      char c;
+      // za ':' je ' ' a nasleduje jmeno cpu a konec radku
+      char c = fgetc(cpuinfo);
       while((c = fgetc(cpuinfo)) != '\n')
         name[i++] = c;
       name[i] = '\0';
       break;
     }
+    // TARGET v textu neni -> doctu do konce radku
     else {
-      do {} while(fgetc(cpuinfo) != '\n');
+      while(fgetc(cpuinfo) != '\n');
     }
     
   }
